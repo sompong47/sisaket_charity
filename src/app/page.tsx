89a2159b3d994ac7d15f280e5b18010e9223dab1
sompong47 <maserxtv@gmail.com>
2025-11-12 17:28:05 +0700

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,7 +55,7 @@ export default function Home() {
             className={styles.secondary}
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
-            rel="noopener noreferrer"
+            rel="noopenerb noreferrer"
           >
             Documentation
           </a>
